21343a8c2745150f7d52122aebf64b94676d5ece
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2019-10-12 21:22:31 -0700

    Edited

---
+++ questions.js
@@ -0,0 +1,29 @@
+  var questions = [
+    {
+        title: "Grand Central Terminal, Park Avenue, New York is the world's",
+        choices: ["largest railway station", "highest railway station", "longest railway station", "none of the above"],
+        answer: "largest railway station"
+    },
+    {
+        title: "Entomology is the science that studies",
+        choices: ["Behavior of human beings", "Insects", "The origin and history of technical and scientific terms", "The formation of rocks"],
+        answer: "Insects"
+    },
+    {
+        title: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+        choices: ["Asia", "Africa", "Europe", "Australia"],
+        answer: "Africa"
+    },
+    {
+        title: "Garampani sanctuary is located at",
+        choices: ["Junagarh, Gujarat", "Diphu, Assam", "Kohima, Nagaland", "Gangtok, Sikkim"],
+        answer: "Diphu, Assam"
+    },
+    {
+        title: "For which of the following disciplines is Nobel Prize awarded?",
+        choices: ["Physics and Chemistry", "Physiology or Medicine", "	Literature, Peace and Economics", "All of the above"],
+        answer: "All of the above"
+    },
+
+  ];
+  
---
+++ script.js
@@ -0,0 +1,54 @@
+
+var questionsEl = document.querySelector("#questions");
+var qIndex = 0;
+var oIndex = 0;
+var timeEl = document.querySelector(".time");
+var correctAnwer = 0;
+var wrongAnswer = 0;
+
+
+var secondsLeft = 65;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = "Time: "+ secondsLeft;
+  
+      var 
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+    console.log("test");
+  };
+    setTime ();
+
+ function startQuiz() {
+    questionsEl.innerHTML = questions[qIndex].title;
+    
+     for (var i=0; i < questions[qIndex].choices.length;i++) {
+        var button = document.createElement("button")
+        button.textContent = questions[qIndex].choices[i];
+        // var container = document.querySelector("#container");
+        button.addEventListener("click", function(event) {
+        var userGuess = event.target.textContent;
+        if (userGuess === questions[qIndex].answer) {
+        alert("Correct!");     
+        }
+        else {
+            alert("False");
+        }
+        qIndex++;
+        return startQuiz();
+        })
+        container.appendChild(button)
+    }
+ }
+startQuiz()
+
+
+
+
+// alert("Total correct " + correct + "\nTotal ties: " + ties + "\nTotal losses: " + losses);
